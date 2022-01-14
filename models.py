78a05b90e45093cf50ad723283b59d3fbd60cdcc
Raghav Sharma <raghav_tinker@yahoo.com>
2022-01-14 23:28:52 +0530

    setup command made...database interaction setup done

---
+++ models.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.sqltypes import Boolean
+from database import Base
+
+try:
+    PREFIX = os.environ["PREFIX"]
+except:
+    PREFIX = "*"
+
+class Clients(Base):
+    __tablename__ = 'clients'
+    id = Column(Integer, primary_key=True, index=True)
+    guild_id = Column(Integer, index=True, nullable=False)
+    prefix = Column(String, default=PREFIX)
+    channel = Column(String, index=True, nullable=False)
+
+    def __init__(self, guild_id, channel, prefix=PREFIX):
+        self.guild_id = guild_id
+        self.prefix = prefix
+        self.channel = channel
