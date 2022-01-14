1cc8254343cc8adb6cb74515f1a9056503e1fd38
Raghav Sharma <raghav_tinker@yahoo.com>
2022-01-15 01:20:03 +0530

    random glitch solved

--- Bot/cogs/random.py
+++ Bot/cogs/random.py
@@ -6,16 +6,30 @@ import random
 class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
     @commands.command(name='random', aliases=['r']) 
     async def random(self, ctx):
+        count = 0
         url = 'https://xkcd.com/info.0.json'
         response = requests.get(url)
         max_num = response.json()["num"]
         rand_num = random.randint(1, max_num)
-        url = 'https://xkcd.com/' + str(rand_num) + '/info.0.json'
-        response = requests.get(url)
-        embed = discord.Embed(title=response.json()["title"], description=response.json()["alt"], color=0x00ff00)
-        embed.set_image(url=response.json()["img"])
+        embed = None
+        while count < 5:
+            url = 'https://xkcd.com/' + str(rand_num) + '/info.0.json'
+            try:
+                response = requests.get(url)
+            except:
+                count = count + 1
+                continue
+        
+            embed = discord.Embed(title=response.json()["title"], description=response.json()["alt"], color=0x00ff00)
+            embed.set_image(url=response.json()["img"])
+            break
+
+        if embed is None:
+            embed = discord.Embed(title="Couldn't fetch comic", description="", color=0x00ff00)
         await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Random(bot))
